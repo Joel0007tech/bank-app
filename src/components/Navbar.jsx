99d574d778cf123454d0a10ff912f390802a7acd
Joel <ajiolajoel@gmail.com>
2023-03-13 23:58:12 +0100

    update to the project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,17 @@ const Navbar = () => {
 
   return (
     <nav className="w-full  py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <img
+        src={logo}
+        alt="hoobank"
+        className="w-[124px] h-[32px] relative top-5"
+      />
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+              index === navLinks.length - 1 ? "mr-0" : "mr-5"
             } text-white
             mr-10`}
           >
@@ -28,7 +32,7 @@ const Navbar = () => {
         <img
           src={toggle ? close : menu}
           alt="menu"
-          className="w-[28px] h-[28px] object-contain relative bottom-6"
+          className="w-[28px] h-[28px] object-contain relative bottom-3"
           onClick={() => setToggle((prev) => !prev)}
         />
         <div
